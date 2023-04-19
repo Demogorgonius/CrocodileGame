765201c7f3129b06423179fff7a843e68486a728
AndreyMikhailov <mands315@gmail.com>
2023-04-19 22:15:59 +0300

    Create RightView and RightViewController

---
+++ CrocodileGame/Controllers/RightViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RightViewController.swift
+//  CrocodileGame
+//
+//  Created by Andrey on 19.04.2023.
+//
+
+import UIKit
+
+class RightViewController: CustomViewController<RightView> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ CrocodileGame/View/RightView.swift
@@ -0,0 +1,22 @@
+//
+//  RightView.swift
+//  CrocodileGame
+//
+//  Created by Andrey on 19.04.2023.
+//
+
+import UIKit
+
+class RightView: CustomView {
+    
+    override func setViews() {
+        super.setViews()
+        
+    }
+    
+    override func layoutViews() {
+        super.layoutViews()
+        
+    }
+    
+}

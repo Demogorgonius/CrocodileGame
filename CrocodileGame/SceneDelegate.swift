6648507b90a432ab011959743a449ea52fffacda
Anastasiya <ios.developer.by@gmail.com>
2023-04-20 18:32:19 +0300

    создала startViewController

--- CrocodileGame/SceneDelegate.swift
+++ CrocodileGame/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let navigationVC = UINavigationController()
-        let mainVC = ViewController()
+        let mainVC = StartViewController()
         navigationVC.viewControllers = [mainVC]
         window?.rootViewController = navigationVC
         window?.makeKeyAndVisible()

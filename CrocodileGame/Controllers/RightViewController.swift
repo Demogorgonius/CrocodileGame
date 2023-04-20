e0b33c0df2acff621b9508ebc741e546328b5d20
AndreyMikhailov <mands315@gmail.com>
2023-04-20 18:48:24 +0300

    Preview version UI RightView

--- CrocodileGame/Controllers/RightViewController.swift
+++ CrocodileGame/Controllers/RightViewController.swift
@@ -11,6 +11,24 @@ class RightViewController: CustomViewController<RightView> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.hidesBackButton = true
+        customView.delegate = self
+        
+        customView.setTeamAvatar(icon: "🤷🏻", background: CrocodileColors.blue.setColor)
+        customView.setTeamName(name: "Cowboys")
+        customView.setTeamPoints(points: 1)
+        customView.setNextTeamLabel(team: "Стройняшки")
     }
     
+}
+
+extension RightViewController: RightViewDelegate {
+    func RightView(_ view: RightView, didTapButton button: UIButton) {
+        let buttonIdentifier = button.restorationIdentifier ?? ""
+        
+        if buttonIdentifier == "NextTeam" {
+            // Move to GameViewController
+        }
+    }
 }

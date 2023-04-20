6a092c1dfe97fc219539dbe1ed263925c904e24e
jstted <tedeev17@gmail.com>
2023-04-20 21:05:45 +0300

    Done GameResultView and GameResultViewController

---
+++ CrocodileGame/Controllers/GameResultViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GameResultViewController.swift
+//  CrocodileGame
+//
+//  Created by Mikhail Tedeev on 20.04.2023.
+//
+
+import Foundation
---
+++ CrocodileGame/View/GameResultView.swift
@@ -0,0 +1,8 @@
+//
+//  GameResultView.swift
+//  CrocodileGame
+//
+//  Created by Mikhail Tedeev on 20.04.2023.
+//
+
+import Foundation
